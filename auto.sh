685ba7652cc90fbdf056891f08277d676c3ded89
Jonas Weber <github@jonasw.de>
2016-08-26 22:03:31 +0200

    print error message when create fails

--- auto.sh
+++ auto.sh
@@ -57,7 +57,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master" -a "$TRAV
 
 	echo "---- create repositories on quay ----"
 	for i in $AFFECTED; do
-		echo -n "* $i"
+		echo -n "* $i : "
 		(
 			echo "{"
 			echo '"namespace": "'$NAMESPACE'",'
@@ -70,7 +70,8 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master" -a "$TRAV
 			-HAuthorization:Bearer\ $QUAY_TOKEN \
 			-d @- \
 			-HContent-Type:application/json \
-			https://quay.io/api/v1/repository
+			https://quay.io/api/v1/repository \
+			|| echo "  FAILED!"
 		echo
 	done
 
