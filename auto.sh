1d7e48964fc2f30f0898523bd2c93a0add7646ba
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-09-04 12:28:55 +0200

    use new python based helpers

--- auto.sh
+++ auto.sh
@@ -18,42 +18,13 @@ function init() {
 	bzip2 -d repodata.json.bz2
 }
 
-function affected_packages() {
-	cd bioconda-recipes
-	git log --name-only --pretty="" --since="25 hours ago" | grep -E '^recipes/.*/meta.yaml' | sed 's#recipes/\([^/]*\)/.*#\1#' | sort | uniq
-	cd ..
-}
-
-function condaversions_for() {
-	PKG=$1
-	jq '.packages|map(select(.name == "'$1'"))|map(.version + "--" + .build)' repodata.json
-}
-
-function quayversions_for() {
-	PKG=$1
-	(wget --quiet -O - "https://quay.io/api/v1/repository/mulled/$PKG" || echo '{"tags": {}}') | jq '.tags | keys'
-}
-
-function new_versions_for() {
-	PKG=$1
-	CONDA=$(condaversions_for $PKG)
-	QUAY=$(quayversions_for $PKG)
-
-	jq -n "$CONDA - $QUAY" 
-}
-
-function involucro_params_for() {
-	PKG=$1
-	new_versions_for $1 | jq --raw-output 'map("-set PACKAGE='$PKG' -set TAG=" + . + " -set VERSION=" + split("--")[0] + " -set BUILD=" + split("--")[1]) | join("\n")'
-}
-
 set -e
 init
 
-AFFECTED=$(affected_packages)
+AFFECTED=$(./helpers.py)
 
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	COMMANDS="build push"
+	COMMANDS="all"
 
 	echo "---- create repositories on quay ----"
 	for i in $AFFECTED; do
@@ -81,4 +52,9 @@ fi
 
 echo "---- package apps ----"
 
-(for i in $(affected_packages); do involucro_params_for $i ; done) | xargs -I XX /bin/sh -c "./involucro XX $COMMANDS"
+IFS=$'\n'
+for i in $(./helpers.py $COMMANDS)
+    do 
+        echo 'Running the following command:' $i
+        eval $i
+    done
